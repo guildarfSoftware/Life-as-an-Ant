234e710debe3b5980cbcfbf69e04a502ebe71f55
guildarfSoftware <68065345+guildarfSoftware@users.noreply.github.com>
2021-01-20 17:22:42 +0100

    Implemented Leader-follower system

---
+++ Assets/Scripts/Control/Follower.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using RPG.Combat;
+using RPG.Core;
+using RPG.Harvest;
+using RPG.Movement;
+using RPG.Pheromones;
+using UnityEngine;
+
+namespace RPG.Control
+{
+    public class Follower : MonoBehaviour
+    {
+        EntityDetector detector;
+        PheromoneFollower pheromoneFollower;
+        Harvester harvester;
+        Fighter fighter;
+        Explorer explorer;
+        PheromoneGenerator pheromoneGenerator;
+        Mover mover;
+        GameObject target;
+
+        GameObject nest, player;
+        AntState currentState;
+        float targetRange = 2f;
+        float followRange= 5f;
+        enum AntState   //ordere by priority
+        {
+            notifying,
+            attacking,
+            storing,
+            harvesting,
+            following,
+        }
+
+        private void Start()
+        {
+            pheromoneGenerator = GetComponent<PheromoneGenerator>();
+            detector = GetComponent<EntityDetector>();
+            pheromoneFollower = GetComponent<PheromoneFollower>();
+            harvester = GetComponent<Harvester>();
+            fighter = GetComponent<Fighter>();
+            explorer = GetComponent<Explorer>();
+            mover=  GetComponent<Mover>();
+
+            nest = GameObject.FindGameObjectWithTag("Nest");
+            player = GameObject.FindGameObjectWithTag("Player");
+
+            DefaultState();
+        }
+        private void Update()
+        {
+            if(target == null) DefaultState();
+            switch (currentState)
+            {
+                case AntState.notifying:
+                    {
+                        mover.MoveTo(target.transform.position);
+                        if(GetDistance(target) < targetRange)
+                        {
+                            pheromoneGenerator.StopGeneration();
+                            DefaultState();
+                        }
+                        break;
+                    }
+                case AntState.attacking:
+                    {
+                        fighter.Attack(target);
+                        if(target.transform.GetComponent<Health>().IsDead)
+                        {
+                            DefaultState();
+                        }
+                        break;
+                    }
+                case AntState.storing:
+                    {
+                        harvester.Store(target);
+
+                        if(harvester.IsEmpty)
+                        {
+                            DefaultState();
+                        }
+
+                        break;
+                    }
+                case AntState.harvesting:
+                    {
+                        harvester.Harvest(target);
+                        if(harvester.IsFull || target.GetComponent<HarvestTarget>().IsEmpty)
+                        {
+                            Store(nest);
+                        }
+
+                        break;
+                    }
+                case AntState.following:
+                    {
+                        if(target == null) target = player;
+
+                        Vector3 followPosition = GetFollowPosition(target); 
+                        mover.MoveTo(followPosition);
+                        break;
+                    }
+                default: 
+                 {
+                     DefaultState();
+                     break;
+                 }
+            }
+        }
+
+        private Vector3 GetFollowPosition(GameObject target)    // follows at exactly follow rango to avoid followers making barrier
+        {
+            Vector3 followDirection = target.transform.position - transform.position;
+            followDirection.Normalize();
+            return target.transform.position - followDirection * followRange;
+        }
+
+        private float GetDistance(GameObject target)
+        {
+            return Vector3.Distance(target.transform.position,transform.position);
+        }
+
+        internal void Harvest(GameObject target)
+        {
+            this.target = target;
+            ChangeState(AntState.harvesting);
+            harvester.Harvest(target);
+        }
+        internal void Attack(GameObject target)
+        {
+            this.target = target;
+            ChangeState(AntState.attacking);
+            fighter.Attack(target);
+        }
+        internal void NotifyNest(PheromoneType pheromoneType)
+        {
+            target = nest;
+            ChangeState(AntState.notifying);
+            pheromoneGenerator.StartGeneration(pheromoneType);
+            GetComponent<Mover>().MoveTo(target.transform.position);
+        }
+
+        public void Store(GameObject target)
+        {
+            this.target = target;
+            ChangeState(AntState.storing);
+            harvester.Store(target);
+        }
+
+        public void DefaultState()
+        {
+            target=player;
+            currentState = AntState.following;
+        }
+
+        private void ChangeState(AntState newState)
+        {
+            if (newState < currentState)
+            {
+                GetComponent<ActionScheduler>().CancelCurrentAction();
+                currentState = newState;
+            }
+        }
+
+
+    }
+
+}
---
+++ Assets/Scripts/Control/Leader.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RPG.Pheromones;
+using RPG.Core;
+
+namespace RPG.Control
+{
+    public class Leader : MonoBehaviour
+    {
+        public int maxFollowers = 2;
+        List<Follower> followers;
+
+        Follower notifier;    //will be in charge of sending pheromone signals to the nest
+
+
+        private void Start()
+        {
+            followers = new List<Follower>();
+        }
+
+        private void Update()
+        {
+            if (followers.Count < maxFollowers)
+            {
+                GameObject newFollower = FindCloseWorkerAnt();
+                if (newFollower != null)
+                {
+                    AddFollower(newFollower);
+                }
+            }
+        }
+
+        GameObject FindCloseWorkerAnt()
+        {
+            EntityDetector detector = GetComponent<EntityDetector>();
+            if (detector == null) return null;
+            var workers = detector.GetEntitiesWithTag("Worker");
+
+            foreach (var worker in workers)
+            {
+                Follower follower = worker.GetComponent<Follower>();
+                if (follower == null)
+                {
+                    return worker;  //need an ant that is not already a follower
+                }
+            }
+            return null;
+        }
+
+        public void AddFollower(GameObject followerObject)  // replaces the worker controller of the worker with follower controller and ads it to the follower list
+        {
+            if (followerObject == null) return;
+
+            WorkerController controller = followerObject.GetComponent<WorkerController>();
+            if (controller != null)
+            {
+                Destroy(controller);
+            }
+
+            Follower follower = followerObject.GetComponent<Follower>();
+
+            if (follower == null)
+            {
+                follower = followerObject.AddComponent<Follower>();
+            }
+
+            followers.Add(follower);
+            if (notifier == null) notifier = follower;
+        }
+
+        public void RemoveFollower(GameObject followerObject)
+        {
+            if (followerObject == null) return;
+
+            Follower follower = followerObject.GetComponent<Follower>();
+
+            if (follower == null) return;
+
+            if (followers.Contains(follower))
+            {
+                followers.Remove(follower);
+                if (follower == notifier)
+                {
+                    notifier = GetFirstFollower();  //replaces the notifier or sets it null if no other follower available
+                }
+            }
+        }
+
+        internal void CommandStore(GameObject gameObject)
+        {
+            foreach (Follower follower in followers)
+            {
+                follower.Store(gameObject);
+            }
+        }
+
+        Follower GetFirstFollower()
+        {
+            if(followers.Count == 0)  return null;
+            return followers[0];
+        }
+
+        public void CommandHarvest(GameObject gameObject)
+        {
+            foreach (Follower follower in followers)
+            {
+                follower.Harvest(gameObject);
+            }
+        }
+
+        public void CommandAttack(GameObject gameObject)
+        {
+            foreach (Follower follower in followers)
+            {
+                if (follower == notifier)
+                {
+                    follower.NotifyNest(PheromoneType.Combat);
+                }
+                else
+                {
+                    follower.Attack(gameObject);
+                }
+            }
+        }
+    }
+
+}
