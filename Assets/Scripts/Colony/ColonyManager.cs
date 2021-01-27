a605a9acf2de8a6cf83a11ce76da79200c329b6a
guildarfSoftware <68065345+guildarfSoftware@users.noreply.github.com>
2021-01-27 15:20:36 +0100

    Created Colony manager to handle resource storage and population

---
+++ Assets/Scripts/Colony/ColonyManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RPG.Harvest;
+using System;
+
+namespace RPG.Colony
+{
+    [RequireComponent(typeof(Storage))]
+    public class ColonyManager : MonoBehaviour
+    {
+        public Storage storage { get; private set; }
+        List<GameObject> ants =  new List<GameObject>();
+
+        [SerializeField] GameObject workerPrefab;
+        [SerializeField] int startingAnts=0;
+        // Start is called before the first frame update
+        void Start()
+        {
+            storage = GetComponent<Storage>();
+            for (int i = 0; i < startingAnts; i++)
+            {
+                CreateWorker();
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public bool CreateWorker()
+        {
+            if (storage.storedAmount < 30) return false;
+
+            storage.Consume(30);
+            GameObject newAnt = GameObject.Instantiate(workerPrefab);
+            newAnt.transform.position = transform.position;
+            newAnt.transform.parent = transform;
+            ants.Add(newAnt);
+
+            return true;
+        }
+    }
+}
