0c0980650568748d84f150e1ef3346fdf3b24489
guildarfSoftware <68065345+guildarfSoftware@users.noreply.github.com>
2021-02-04 15:19:02 +0100

    Created scriptable object class with upgrade data

---
+++ Assets/Scriptable Objects/Upgrade.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.UI
+{
+    public enum BonusElement
+    {
+        None,
+        Storage,
+        Population,
+        Followers,
+        Health,
+        Speed,
+        CarryCapacity,
+        Damage,
+        Worker,
+        Princess,
+    }
+
+    [CreateAssetMenu(fileName = "Upgrade")]
+    public class Upgrade : ScriptableObject
+    {
+
+        public string upgradetext;
+        public float bonus;
+
+        public BonusElement bonusElement;
+
+        public float upgradeTime;
+        public int foodCost, workerCost;
+
+        public override string ToString()
+        {
+            string s = name;
+            s += "( " + foodCost + " food ";
+            if (workerCost != 0)
+            {
+                s += "/n" + workerCost + "ants";
+            }
+            if (upgradeTime != 0)
+            {
+                s += "for " + upgradeTime + "seconds";
+            }
+
+            s += ")";
+            return s;
+        }
+    }
+}
