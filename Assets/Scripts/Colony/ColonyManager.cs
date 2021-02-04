f17f2e2754cdbcd45623b130b7715e70b933612d
guildarfSoftware <68065345+guildarfSoftware@users.noreply.github.com>
2021-02-04 15:17:25 +0100

    Update Colony manager to handle upgrades and buy workers

--- Assets/Scripts/Colony/ColonyManager.cs
+++ Assets/Scripts/Colony/ColonyManager.cs
@@ -4,34 +4,119 @@ using UnityEngine;
 using RPG.Harvest;
 using System;
 using RPG.Map;
+using RPG.Control;
 
 namespace RPG.Colony
 {
     [RequireComponent(typeof(Storage))]
     public class ColonyManager : MonoBehaviour
     {
+        static ColonyManager instance;
         public Storage storage { get; private set; }
-        List<GameObject> ants = new List<GameObject>();
 
+        int maxPopulation = 15;
+        List<GameObject> allAnts = new List<GameObject>();
+        List<GameObject> availableAnts = new List<GameObject>();
+        List<GameObject> buildingAnts = new List<GameObject>();
+        List<GameObject> followerAnts = new List<GameObject>();
+        GameObject playerAnt;
+        Leader leader;
         [SerializeField] GameObject workerPrefab;
         [SerializeField] int startingAnts = 0;
+
+        public void IncreaseMaxStorage(int storageBonus)
+        {
+            storage.IncreaseMaxCapacity(storageBonus);
+        }
+
         // Start is called before the first frame update
         void Start()
         {
+            if (instance != null) Debug.LogWarning("Warning: multiple colonyManager instances active");
+            instance = this;
+
+            playerAnt = GameObject.FindGameObjectWithTag("Player");
+            if (playerAnt != null) leader = playerAnt.GetComponent<Leader>();
             storage = GetComponent<Storage>();
+
             for (int i = 0; i < startingAnts; i++)
             {
                 CreateWorker();
             }
         }
 
-        public bool BuyWorker()
+        private void Update()
         {
-            if (storage.storedAmount < 30) return false;
-            CreateWorker();
-            storage.Consume(30);
+            if (leader != null)
+            {
+                if (leader.needsFollower && availableAnts.Count > 0)
+                {
+                    GameObject ant = availableAnts[availableAnts.Count - 1];
+                    leader.AddFollower(ant);
+                    followerAnts.Add(ant);
+                    availableAnts.RemoveAt(availableAnts.Count - 1);
+                }
 
-            return true;
+            }
+        }
+
+        void StartBuildingProcess(int workersNeeded, float releaseTime)   //@todo make this start only when enought ants are waiting on the nest;
+        {
+            int foundWorkers = 0, i = 0;
+
+            while (foundWorkers < workersNeeded && i < availableAnts.Count)
+            {
+                GameObject ant = availableAnts[i];
+                if (ant.activeSelf == true)
+                {
+                    DeactivateAntForSeconds(ant, releaseTime);
+                    availableAnts.RemoveAt(i);
+                    foundWorkers++;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+
+            if (foundWorkers == workersNeeded) return;
+
+            i = 0;
+            while (foundWorkers < workersNeeded && i < followerAnts.Count) // if available ants where not enought use also Follower ants
+            {
+                GameObject ant = followerAnts[i];
+                if (ant.activeSelf == true)
+                {
+                    foundWorkers++;
+                    leader.RemoveFollower(ant);
+                    DeactivateAntForSeconds(ant, releaseTime);
+                    followerAnts.RemoveAt(i);
+                }
+                else
+                {
+                    i++;
+                }
+            }
+
+        }
+
+        void DeactivateAntForSeconds(GameObject ant, float reactivateTime)
+        {
+            ant.SetActive(false);
+            buildingAnts.Add(ant);
+
+            Invoke("ReactivateWorker", reactivateTime);
+        }
+
+        void ReactivateWorker()
+        {
+            if (buildingAnts.Count != 0)
+            {
+                GameObject reactivatedAnt = buildingAnts[buildingAnts.Count - 1];
+                reactivatedAnt.SetActive(true);
+                buildingAnts.RemoveAt(buildingAnts.Count - 1);
+                availableAnts.Add(reactivatedAnt);
+            }
         }
 
         private void CreateWorker()
@@ -41,7 +126,57 @@ namespace RPG.Colony
             spawnPos.y = MapTools.getTerrainHeight(spawnPos);
             newAnt.transform.position = spawnPos;
             newAnt.transform.parent = transform;
-            ants.Add(newAnt);
+            allAnts.Add(newAnt);
+            availableAnts.Add(newAnt);
         }
+
+        public static void ApplyCost(int foodCost, int workerCost, float upgradeTime)
+        {
+            instance.storage.Consume(foodCost);
+            instance.StartBuildingProcess(workerCost, upgradeTime);
+        }
+
+        public static bool CheckCost(int foodCost, int workerCost)
+        {
+            if (foodCost > instance.storage.storedAmount)
+            {
+                print("not enought food");
+                return false;
+            }
+            if (workerCost > instance.availableAnts.Count + instance.followerAnts.Count)
+            {
+                print("not enought workers");
+                return false;
+            }
+            return true;
+        }
+
+        public static void IncreaseMaxStorage(float bonus)
+        {
+            instance.storage.IncreaseMaxCapacity(bonus);
+        }
+
+
+        public static void IncreaseMaxPopulation(int bonus)
+        {
+            instance.maxPopulation += bonus;
+        }
+
+        public static void IncreaseMaxFollowers(int bonus)
+        {
+            instance.leader.maxFollowers+=bonus;
+        }
+
+        public static void AddWorker()
+        {
+            instance.CreateWorker();
+        }
+
+        public static void AddPrincess()
+        {
+            throw new NotImplementedException();
+        }
+
+
     }
 }
--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -3,41 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 using RPG.Core;
 using RPG.Colony;
+using System;
 
 namespace RPG.UI
 {
     public class UIManager : MonoBehaviour
     {
+        public static UIManager instance;
         GameObject player;
         Health playerHealth;
-
-        GameObject nest;
+        [SerializeField] GameObject upgradesMenu;
         ColonyManager colony;
 
         // Start is called before the first frame update
         void Start()
         {
+            instance = this;
             player = GameObject.FindWithTag("Player");
             if (player != null)
             {
                 playerHealth = player.GetComponent<Health>();
-                if(playerHealth!= null) playerHealth.OnDamaged+=UpdateHealthBar;
+                if (playerHealth != null) playerHealth.OnDamaged += UpdateHealthBar;
             }
 
-            nest= GameObject.FindWithTag("Nest");
-            if(nest!=null)
+            GameObject nest = GameObject.FindWithTag("Nest");
+            if (nest != null)
             {
                 colony = nest.GetComponent<ColonyManager>();
-                if(colony != null) colony.storage.onStoreChange += UpdateStorageText;
+                if (colony != null) colony.storage.onStoreChange += UpdateStorageText;
             }
-
-
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-
         }
 
         void UpdateHealthBar()
@@ -49,23 +43,24 @@ namespace RPG.UI
 
         void UpdateStorageText()
         {
-            if(colony == null) return;
-            UIStorageText.instance.SetValue(colony.storage.storedAmount);
-        }
-
-        // called in button onClick from editor
-        public void TryBuyWorker()
-        {
-            if(!colony.BuyWorker())
-            {
-                print("Not enought food");
-            }
+            if (colony == null) return;
+            UIStorageText.instance.SetValue((int)colony.storage.storedAmount);
         }
 
         private void OnDisable()
         {
             if (playerHealth != null) playerHealth.OnDamaged -= UpdateHealthBar;
             if (colony != null) colony.storage.onStoreChange += UpdateStorageText;
+        }
+
+        public void EnableUpgradesMenu()
+        {
+            upgradesMenu.SetActive(true);
+        }
+
+        public void DisableUpgradesMenu()
+        {
+            upgradesMenu.SetActive(false);
         }
     }
 }
