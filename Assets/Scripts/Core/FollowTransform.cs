a697c225b3e650707f1ea70c7e028793e2a6eaaa
guildarfSoftware <68065345+guildarfSoftware@users.noreply.github.com>
2021-05-20 13:07:02 +0200

    Added camera controls with mouse wheel and mouse wheel click

--- Assets/Scripts/Core/FollowTransform.cs
+++ Assets/Scripts/Core/FollowTransform.cs
@@ -4,18 +4,35 @@ namespace RPG.Core
 {
     public class FollowTransform : MonoBehaviour
     {
-        [SerializeField]
-        public Transform target;
-        // Start is called before the first frame update
-        void Start()
-        {
+        [SerializeField] Transform target;
 
-        }
+        [SerializeField] Vector3 cameraOffset;
+        [SerializeField] float rotationSpeed = 1f;
 
-        // Update is called once per frame
+        [SerializeField] float minFov = 15f;
+        [SerializeField] float maxFov = 90f;
+        [SerializeField] float sensitivity = 10f;
+
         void LateUpdate()
         {
-            transform.position = target.position;
+
+            if (Input.GetMouseButton(2))
+            {
+                Quaternion turnAngle = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * rotationSpeed, transform.up);
+                cameraOffset = turnAngle * cameraOffset;
+                turnAngle = Quaternion.AngleAxis(Input.GetAxis("Mouse Y") * rotationSpeed, -transform.right);
+                cameraOffset = turnAngle * cameraOffset;
+            }
+
+            {
+                float fov = Camera.main.fieldOfView;
+                fov -= Input.GetAxis("Mouse ScrollWheel") * sensitivity;
+                fov = Mathf.Clamp(fov, minFov, maxFov);
+                Camera.main.fieldOfView = fov;
+            }
+
+            transform.position = target.position + cameraOffset;
+            transform.LookAt(target);
         }
     }
 
