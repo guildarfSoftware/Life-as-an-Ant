f263083a3eb66c1d3c17e0640263a5efbb0c1c43
guildarfSoftware <68065345+guildarfSoftware@users.noreply.github.com>
2021-05-24 11:19:08 +0200

    Separate follow transform script and camera controls script

---
+++ Assets/CameraControls.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControls : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [SerializeField] Vector3 cameraOffset;
+    [SerializeField] float rotationSpeed = 1f;
+    [SerializeField] float minFov = 15f;
+    [SerializeField] float maxFov = 90f;
+    [SerializeField] float sensitivity = 10f;
+
+    void LateUpdate()
+    {
+
+        if (Input.GetMouseButton(2))
+        {
+            Quaternion turnAngle = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * rotationSpeed, transform.up);
+            cameraOffset = turnAngle * cameraOffset;
+            turnAngle = Quaternion.AngleAxis(Input.GetAxis("Mouse Y") * rotationSpeed, -transform.right);
+            cameraOffset = turnAngle * cameraOffset;
+        }
+
+        {
+            float fov = Camera.main.fieldOfView;
+            fov -= Input.GetAxis("Mouse ScrollWheel") * sensitivity;
+            fov = Mathf.Clamp(fov, minFov, maxFov);
+            Camera.main.fieldOfView = fov;
+        }
+
+        transform.position = target.position + cameraOffset;
+        transform.LookAt(target);
+    }
+}
--- Assets/Scripts/Core/FollowTransform.cs
+++ Assets/Scripts/Core/FollowTransform.cs
@@ -6,33 +6,9 @@ namespace RPG.Core
     {
         [SerializeField] Transform target;
 
-        [SerializeField] Vector3 cameraOffset;
-        [SerializeField] float rotationSpeed = 1f;
-
-        [SerializeField] float minFov = 15f;
-        [SerializeField] float maxFov = 90f;
-        [SerializeField] float sensitivity = 10f;
-
         void LateUpdate()
         {
-
-            if (Input.GetMouseButton(2))
-            {
-                Quaternion turnAngle = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * rotationSpeed, transform.up);
-                cameraOffset = turnAngle * cameraOffset;
-                turnAngle = Quaternion.AngleAxis(Input.GetAxis("Mouse Y") * rotationSpeed, -transform.right);
-                cameraOffset = turnAngle * cameraOffset;
-            }
-
-            {
-                float fov = Camera.main.fieldOfView;
-                fov -= Input.GetAxis("Mouse ScrollWheel") * sensitivity;
-                fov = Mathf.Clamp(fov, minFov, maxFov);
-                Camera.main.fieldOfView = fov;
-            }
-
-            transform.position = target.position + cameraOffset;
-            transform.LookAt(target);
+            transform.position = target.position;
         }
     }
 
